e424a979c3bdda622b31e8ed77459a6dac399307
Ben <ben@notionparallax.co.uk>
2019-12-20 07:25:06 +1100

    first

---
+++ goodreads_note.py
@@ -0,0 +1,251 @@
+#%%
+import requests as r
+import json
+import xmltodict
+import pandas as pd
+from dateutil import parser
+import matplotlib.pyplot as plt
+import datetime
+import math
+
+# %%
+tb = pd.read_excel("modified_books.xlsx")
+
+tb.sort_values(by="dt_started_at", ascending=False, inplace=True)
+tb.reset_index(drop=True, inplace=True)
+#%%
+plt.rcParams["figure.figsize"] = (8.27, 11.69)
+fig, ax = plt.subplots()
+for index, row in tb.iterrows():
+    marker = "-"
+
+    ax.plot_date(
+        [row.dt_started_at, row.dt_read_at],
+        [index, index],
+        fmt=marker,
+        tz=None,
+        xdate=True,
+        ydate=False,
+        lw=2.5,
+    )
+    ax.text(
+        row.dt_started_at,
+        index,
+        f"{row.author_1_name}: {row.title}",
+        fontsize=1.5,
+        verticalalignment="center",
+    )
+
+fig.autofmt_xdate()
+ax.set_xlim([datetime.date(2013, 1, 1), datetime.date(2021, 1, 1)])
+plt.tick_params(axis="y", which="both", left=False, right=False, labelleft=False)
+plt.grid(True)
+plt.title("Books that Ben has read")
+plt.savefig(f"out/bookWaterfall", bbox_inches="tight")
+#%%
+
+cols_to_drop = [
+    "Unnamed: 0",
+    "book",
+    "started_at",
+    "read_at",
+    "date_added",
+    "date_updated",
+    "html_link",
+]
+for c in cols_to_drop:
+    try:
+        tb.drop(c, axis=1, inplace=True)
+    except Exception as e:
+        print(e)
+try:
+    tb["book_data"] = tb.apply(
+        lambda x: eval(x.book.replace("OrderedDict", "dict")), axis=1
+    )
+except:
+    pass
+    # must have already done this step
+#%%
+def dataise(d):
+    if type(d) is str:
+        return eval(d)
+    else:
+        return d
+
+
+tb.book_data = tb.book_data.apply(dataise)
+tb.head()
+#%%
+tb.publication_year.value_counts().sort_index().plot(kind="bar")
+plt.title("Publication year of books read in the last 6ish years")
+plt.ylabel("Count of books")
+plt.xlabel("Year")
+plt.savefig(f"out/publicationYearBar", bbox_inches="tight")
+#%%
+tb.publication_year.hist(bins=50)
+plt.title("Publication year of books read in the last 6ish years")
+plt.ylabel("Count of books")
+plt.xlabel("Year")
+plt.savefig(f"out/publicationYearHist", bbox_inches="tight")
+
+# %%
+[x for x in tb.num_pages if type(x) is not int]
+tb.num_pages.hist(bins=45)
+plt.title("Number of pages in these books")
+plt.ylabel("Count of books")
+plt.xlabel("Number of pages")
+plt.savefig(f"out/numPages", bbox_inches="tight")
+
+#%%
+tb.format.value_counts().plot(kind="bar", rot=20)
+plt.title("Format")
+plt.ylabel("Count of books")
+plt.xlabel("Format of book")
+plt.savefig(f"out/format", bbox_inches="tight")
+#%%
+tb.publisher.value_counts().plot.barh()
+plt.title("Publisher - very unfinished")
+plt.xlabel("Count of books")
+plt.ylabel("Publisher of book")
+plt.savefig(f"out/publisher", bbox_inches="tight")
+#%%
+tb.author_1_name.value_counts()[:30].plot.barh()
+plt.title("First named author, top 30")
+plt.xlabel("Count of books")
+plt.ylabel("Name of first-listed author")
+plt.savefig(f"out/author_top_30", bbox_inches="tight")
+#%%
+tb.rating.hist(bins=4)
+plt.title("My rating")
+plt.ylabel("Count of books")
+plt.xlabel("What I've rated this book")
+plt.savefig(f"out/rating", bbox_inches="tight")
+#%%
+all_authors = []
+for index, row in tb.iterrows():
+    all_authors.append(row.book_data["authors"]["author"])
+authors_df = pd.DataFrame(all_authors)
+authors_df.drop_duplicates(subset="name", inplace=True)
+authors_df.reset_index(drop=True, inplace=True)
+authors_df["image"] = authors_df.apply(lambda x: x.image_url["#text"], axis=1)
+authors_df.average_rating = authors_df.average_rating.apply(float)
+authors_df.ratings_count = authors_df.ratings_count.apply(int)
+authors_df.text_reviews_count = authors_df.text_reviews_count.apply(int)
+cols_to_drop = ["image_url", "role", "small_image_url"]
+for c in cols_to_drop:
+    try:
+        authors_df.drop(c, axis=1, inplace=True)
+    except Exception as e:
+        print(e)
+# authors_df.to_excel("authors.xlsx")
+authors_df.head()
+#%%
+authors_df.average_rating.hist()
+
+# %%
+authors_df.ratings_count.hist()
+#%%
+authors_df.text_reviews_count.hist()
+
+#%%
+"""
+This tries to quantify diversity, clumsily!
+
+Ethnicities from: [Racial and Ethnic Categories and Definitions for NIH Diversity Programs and for Other Reporting Purposes](https://grants.nih.gov/grants/guide/notice-files/not-od-15-089.html)
+American Indian or Alaska Native. A person having origins in any of the original peoples of North and South America (including Central America), and who maintains tribal affiliation or community attachment.
+Asian. A person having origins in any of the original peoples of the Far East, Southeast Asia, or the Indian subcontinent including, for example, Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand, and Vietnam.
+Black or African American. A person having origins in any of the black racial groups of Africa. Terms such as "Haitian" or "Negro" can be used in addition to "Black or African American."
+Hispanic or Latino. A person of Cuban, Mexican, Puerto Rican, South or Central American, or other Spanish culture or origin, regardless of race. The term, "Spanish origin," can be used in addition to "Hispanic or Latino."
+Native Hawaiian or Other Pacific Islander. A person having origins in any of the original peoples of Hawaii, Guam, Samoa, or other Pacific Islands.
+White.
+"""
+adf = pd.read_excel("authors.xlsx")
+adf.head()
+#%%
+adf.sexygendery.value_counts().plot(kind="bar", rot=0)
+plt.title("Gender of unique first authors")
+plt.ylabel("Count of authors")
+plt.xlabel("The gender I've guessed")
+plt.savefig(f"out/Gender_count_of_unique_first_authors", bbox_inches="tight")
+#%%
+adf.ethnicity.value_counts().plot(kind="bar", rot=0)
+plt.title("Ethnicity of unique first authors")
+plt.ylabel("Count of authors")
+plt.xlabel("The ethnicity I've guessed")
+plt.savefig(f"out/first_author_ethnicity", bbox_inches="tight")
+#%%
+adf["compound_diversity"] = adf.apply(
+    lambda x: f"{x.ethnicity}-{x.sexygendery}", axis=1
+)
+adf.compound_diversity.value_counts().plot(kind="bar")
+plt.title('"Compound" diversity of unique first authors')
+plt.ylabel("Count of authors")
+plt.xlabel("joined together")
+plt.savefig(f"out/compundDiversity", bbox_inches="tight")
+#%%
+all_df = tb.merge(adf, right_on="name", left_on="author_1_name")
+all_df.sample(4)
+#%%
+all_df.sexygendery.value_counts().plot(kind="bar", rot=0)
+plt.title("Books read, split by Gender of unique first author")
+plt.ylabel("Count of books")
+plt.xlabel("The gender I've guessed")
+plt.savefig(f"out/gender_of_books_read", bbox_inches="tight")
+#%%
+all_df.ethnicity.value_counts().plot(kind="bar", rot=0)
+plt.title("Ethnicity of unique first authors")
+plt.ylabel("Count of books")
+plt.xlabel("The gender I've guessed")
+plt.savefig(f"out/Ethnicity_of_books_read", bbox_inches="tight")
+#%%
+all_df["reading_year"] = all_df.dt_started_at.apply(lambda x: x.year)
+#%%
+# from : https://stackoverflow.com/a/34919066/1835727
+diversity_data = all_df.groupby(["reading_year", "compound_diversity"]).size().unstack()
+#%%
+diversity_data = diversity_data[
+    [
+        "Asian-Woman",
+        "Black-Woman",
+        "White-Woman",
+        "Asian-Man",  #
+        "Black-Man",
+        "White-Man",
+    ]
+]
+diversity_data.plot(kind="bar", stacked=True, rot=0)
+
+plt.title(
+    "Clumsy analysis of diversity of authors\nfor books read over the last few years"
+)
+plt.ylabel("Count of books")
+plt.xlabel("Year")
+plt.savefig(f"out/clumsyDiversity", bbox_inches="tight")
+
+
+# %%
+fic_data = (
+    all_df.groupby(["reading_year", "ficOrNonFic"])
+    .size()
+    .unstack()
+    .plot(kind="bar", stacked=True, rot=0)
+)
+plt.title("Split between fiction and non fiction by year")
+plt.ylabel("Count of books")
+plt.xlabel("Year")
+plt.savefig(f"out/anualFic_Nonfic", bbox_inches="tight")
+
+# %%
+fic_data = (
+    all_df.groupby(["compound_diversity", "ficOrNonFic"])
+    .size()
+    .unstack()
+    .plot(kind="bar", stacked=True, rot=0)
+)
+plt.title('Split between fiction and non fiction by "compound diversity"')
+plt.ylabel("Count of books")
+plt.xlabel("Somewhat arbitrary Buckets")
+plt.savefig(f"out/compound_diversity_Fic_Nonfic", bbox_inches="tight")
+
+
+# %%
---
+++ new_list_from_GR.py
@@ -0,0 +1,175 @@
+#%%
+import requests as r
+import json
+import xmltodict
+import pandas as pd
+from dateutil import parser
+import matplotlib.pyplot as plt
+import datetime
+import math
+
+
+#%%
+key = "eQNqoldV2y2KH99ZOgqbQ"
+base = "https://www.goodreads.com"
+
+
+#%%
+me_r = r.get("{b}/user/show/19575421.xml?key={k}".format(b=base, k=key))
+xml_data = me_r.content
+me = xmltodict.parse(xml_data)
+
+# print(json.dumps(me, indent=2))
+shelves = me["GoodreadsResponse"]["user"]["user_shelves"]
+shelf = [x for x in shelves["user_shelf"] if x["name"] == "read"][0]
+shelf
+
+
+#%%
+read_shelf = r.get(
+    f"{base}/review/list/19575421.xml?" f"key={key}&v=2" "&shelf=read" "&per_page=200"
+)
+xml_data = read_shelf.content
+read_books = xmltodict.parse(xml_data)
+rb = read_books["GoodreadsResponse"]["reviews"]["review"]
+books = pd.DataFrame(rb)
+
+
+#%%
+books.sample(4)
+
+
+#%%
+books.columns
+
+
+#%%
+books.drop(
+    [
+        "votes",
+        "spoiler_flag",
+        "spoilers_state",
+        "shelves",
+        "recommended_for",
+        "recommended_by",
+        "read_count",
+        "body",
+        "comments_count",
+        "link",
+        "owned",
+    ],
+    axis=1,
+    inplace=True,
+)
+
+
+#%%
+dead_date = parser.parse("2017 12 01 00:00:00 -0800")
+
+
+def parseDateSafe(date):
+    try:
+        if (type(date) is str) and (date is not None):
+            if type(date) is datetime.datetime:
+                return date
+            else:
+                return parser.parse(date).date()
+        else:
+            return dead_date.date()
+    except Exception as e:
+        print(date, e)
+        return date
+
+
+#%%
+books["title"] = books.apply(lambda row: row.book["title_without_series"], axis=1)
+books["author_1_id"] = books.apply(
+    lambda row: row.book["authors"]["author"]["id"], axis=1
+)
+books["author_1_name"] = books.apply(
+    lambda row: row.book["authors"]["author"]["name"], axis=1
+)
+books["dt_updated"] = books.apply(lambda row: parseDateSafe(row.date_updated), axis=1)
+books["dt_started_at"] = books.apply(lambda row: parseDateSafe(row.started_at), axis=1)
+books["dt_read_at"] = books.apply(lambda row: parseDateSafe(row.read_at), axis=1)
+books["dt_added"] = books.apply(lambda row: parseDateSafe(row.date_added), axis=1)
+
+
+#%%
+books.sample(5)
+
+
+#%%
+fig, ax = plt.subplots()
+badRows = []
+
+for index, row in books.iterrows():
+    marker = "x"
+    if (row.dt_started_at != dead_date) and (row.dt_read_at != dead_date):
+        marker = "-"
+        ax.plot_date(
+            [row.dt_started_at, row.dt_read_at],
+            [index, index],
+            fmt=marker,
+            tz=None,
+            xdate=True,
+            ydate=False,
+        )
+        ax.text(
+            row.dt_started_at, index, row.title, fontsize=1, verticalalignment="center"
+        )
+
+    else:
+        badRows.append(row)
+
+
+fig.autofmt_xdate()
+ax.set_xlim([datetime.date(2016, 1, 1), datetime.date(2020, 1, 1)])
+
+plt.show()
+
+#%%
+br = pd.DataFrame(badRows)
+print(br.shape)
+br.head()
+
+#%%
+books["html_link"] = books.apply(
+    lambda row: f"""<tr><td><a href='{row.url}'>{row.title}</a></td></tr>""", axis=1
+)
+
+print("<table>\n", "\n".join(books.html_link), "\n</table>")
+
+
+# %%
+books.to_csv("raw_books.csv")
+
+# def get_publication_year(row):
+#     try:
+#         if row["publication_year"]:
+#             return row["publication_year"]
+#         elif row.book_data:
+#             if row.book_data["publication_year"]:
+#                 return int(row.book_data["publication_year"])
+#             elif row.book_data["published"]:
+#                 return int(row.book_data["published"])
+#             else:
+#                 return 0
+#         else:
+#             return 0
+#     except Exception as e:
+#         print(e, row, "\n")
+
+
+# def get_num_pages(row):
+#     try:
+#         if row["num_pages"] or row.book_data["num_pages"]:
+#             return int(row.book_data["num_pages"])
+#     except:
+#         return 0
+
+# tb["publication_year"] = tb.apply(get_publication_year, axis=1)
+# tb["num_pages"] = tb.apply(get_num_pages, axis=1)
+# tb["format"] = tb.apply(lambda x: x.book_data["format"], axis=1)
+# tb["publisher"] = tb.apply(lambda x: x.book_data["publisher"], axis=1)
+# tb.to_excel("modified_books.xlsx")
